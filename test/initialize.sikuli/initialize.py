e2d6d8e4c50a126bf410e7c00c08e931af567898
Matias Piipari <matias.piipari@gmail.com>
2023-01-29 15:07:05 +0200

    Replace how resizing is done

--- test/initialize.sikuli/initialize.py
+++ test/initialize.sikuli/initialize.py
@@ -5,9 +5,9 @@ opts = OCR.Options()
 opts.textHeight(22)
 # textHeight(Offset(76, 25)
 
-screen.findText("Resize to").click()
+# screen.findText("Resize to").click()
 
-# click("1674910578664.png")
+click("1674910578664.png")
 doubleClick(Pattern("1674911309091.png").targetOffset(48,0))
 type("1920")
 doubleClick(Pattern("1674910635006.png").targetOffset(49,1))
