df4e5b2ed4f251780c8433d5fa0432524965c398
Matias Piipari <matias.piipari@gmail.com>
2023-01-30 16:46:26 +0200

    Adjust the initialization order

--- test/initialize.sikuli/initialize.py
+++ test/initialize.sikuli/initialize.py
@@ -2,24 +2,26 @@ screen = Screen()
 
 opts = OCR.Options()
 opts.textHeight(22)
+
+screen.findText("Options").click()
+click("scale_display.png")
+
+screen.findText("Options").click()
+click("resize_guest.png")
+
+
 # textHeight(Offset(76, 25)
 
 # screen.findText("Resize to").click()
 
-click("1674910578664.png")
-doubleClick(Pattern("1674911309091.png").targetOffset(48,0))
+click("resize_to.png")
+doubleClick(Pattern("width.png").targetOffset(48, 0))
 type("1920")
-doubleClick(Pattern("1674910635006.png").targetOffset(49,1))
+doubleClick(Pattern("height.png").targetOffset(49, 1))
 type("1080")
 
-click("1674913542390.png")
+click("apply.png")
 
-#screen.findText("Options").click()
-click("1675065478360.png")
-
-screen.findText("Options").click()
-click("1675065666811.png")
-
-#wait(3600)
+# wait(3600)
 # screen.findText("Scale display").click()
-#screen.findText("Resize guest to match window size").click()
+# screen.findText("Resize guest to match window size").click()
