d9fc3932668008f31e06bc7eae5222beb4d5b5ab
Matias Piipari <matias.piipari@gmail.com>
2023-01-30 16:58:17 +0200

    Renamed the installer automation images

--- test/installer-automation.sikuli/installer-automation.py
+++ test/installer-automation.sikuli/installer-automation.py
@@ -1,53 +1,48 @@
-# wait("install_ubuntu.png")
-
 wait("english.png", 90)
 
 click("continue.png")
-click("1663606007708.png")
+click("install_ubuntu_banner.png")
 click("continue.png")
 wait("type_here.png")
 click("type_here.png")
 type("dogflap")
 click("continue.png")
 
-click("1675066734616.png")
+click("use_wired_connection.png")
 click("continue.png")
 
-click("1675066757916.png")
+click("minimal_installation.png")
 click("continue.png")
 
-click("1675066774477.png")
+click("erase_disk_and_install_ubuntu.png")
 click("continue.png")
-wait("1660739976913.png")
+wait("write_changes_to_disk.png")
 click("start_installing.png")
 wait("where_are_you.png")
 click("continue.png")
 
-assert "disabled_continue.png"
+wait("disabled_continue.png")
 click("your_name_placeholder.png")
 type("Dustin Henderson")
-click("1660732897015.png")
+click("your_computer_name_dustin.png")
 type("a", Key.CTRL)
 
 type("Falkor")
 
-click("1660733380533.png")
+click("username_dustin_henderson.png")
 type("a", Key.CTRL)
 type("dustin")
 
-click("1660732634756.png")
+click("choose_password.png")
 type("herptyderp")
 
-click("1660732681771.png")
+click("confirm_your_password.png")
 type("herptyderp")
 click("continue.png")
 
 # assert "copying_files.png"
 wait("installation_complete.png", 1800)
-# click("restart_into_ubuntu.png")
-click("1660759005575.png")
+click("shut_down.png")
 
 # findText("Please remove the installation medium, then press ENTER")
-wait("1660758282205.png", 1800)
-# wait("please_remove_installation_medium.png", 1800)
-# type(Key.ENTER)
+wait("remove_installation_medium.png", 60)
